31aaef8ec65c3d6bf1ffe7df321db5a6289e6cd1
Pavle Ćurčić <pavle.curcic678@gmail.com>
2023-10-01 22:04:52 +0200

    +couple of classes ~updating project's name and nullableString = disabled

---
+++ src/anti-task-api/Core/AntiTask.Domain/Task.cs
@@ -0,0 +1,19 @@
+﻿using AntiTask.Domain.Base;
+using AntiTask.Domain.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AntiTask.Domain
+{
+    public class Task : AuditableEntity
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Priority Priority { get; set; }
+        public EstimationUnit EstimationUnit { get; set; }
+    }
+}
